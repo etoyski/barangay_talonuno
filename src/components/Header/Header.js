670591d0543febee24b8a8d9a4a4b87a3d31a2f9
Etoy <solannar03@gmail.com>
2022-05-28 20:52:59 +0800

    update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -81,9 +81,9 @@ const Header = () => {
                        
                        
                        {isloggedin ? ''  : <Tab  sx={{color:'darkgrey'}} onClick={() => navigate('/home')} to="/home" component={Link} label="Home" />} 
-                       {isloggedin ? ''  : <Tab sx={{color:'black'}}  onClick={() => navigate('/about')}to="/about" component={Link} label="About" /> }
+                       <Tab sx={{color:'black'}}  onClick={() => navigate('/about')}to="/about" component={Link} label="About" />
                         <Tab sx={{color:'black'}}  onClick={() => navigate('/services')}to="/services" component={Link} label="Services" />
-                       {isloggedin ? ''  :<Tab sx={{color:'black'}}  onClick={() => navigate('/contactus')}to="/contactus" component={Link} label="Contact Us"/>}
+                       <Tab sx={{color:'black'}}  onClick={() => navigate('/contactus')}to="/contactus" component={Link} label="Contact Us"/>
                        
                        
                         {isloggedin ? <React.Fragment>
