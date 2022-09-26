b37cc0ea0d9709ddc2a2e8c03ddb736c2746a743
unknown <solannar03@gmail.com>
2022-09-26 22:19:08 +0800

    9/25 login request page disable textfield

--- src/pages/Request/request.jsx
+++ src/pages/Request/request.jsx
@@ -59,7 +59,8 @@ export default function Request() {
              phone: inputs.phone,
              purpose: inputs.purpose,
              
-         }, {
+         }, 
+         {
            headers:{
              "Authorization": `Bearer ${localStorage.getItem('T')}`
            }
