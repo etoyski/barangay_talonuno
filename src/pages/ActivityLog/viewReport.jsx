27a31cd26db7b12f1ffd41248f32910dd1075eab
unknown <solannar03@gmail.com>
2022-11-23 22:15:56 +0800

    add pics blotter

--- src/pages/ActivityLog/viewReport.jsx
+++ src/pages/ActivityLog/viewReport.jsx
@@ -189,6 +189,16 @@ const handleFilter = async () => {
     Status:{rep.process}
   </Typography>
 </CardContent>
+<CardContent>
+  <Typography variant="body2" color="text.secondary">
+    Photo:
+  </Typography>
+  <Avatar 
+   variant="square"
+  sx={{height:300, width:300}}
+  src={rep.Image}
+  />
+</CardContent>
 </Collapse>
 
 
